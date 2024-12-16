a4ff42662face9d1214abf38400f20a28eb7ad49
MuhSaktiR <seken876@gmail.com>
2024-12-16 20:02:23 +0700

    Menambahkan getAllMhs untuk mengambil semua data dari DAO

--- app/src/main/java/com/example/activity10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/activity10/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.activity10.repository
 
 import com.example.activity10.data.dao.MahasiswaDao
 import com.example.activity10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,10 @@ class LocalRepositoryMhs (
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    //Dari sini
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
 }
