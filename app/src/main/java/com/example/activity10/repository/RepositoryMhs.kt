88db314069a2288444d32ed8329a71fb99b14a73
MuhSaktiR <seken876@gmail.com>
2024-12-16 19:56:39 +0700

    Menambahkan fungsi getAllMhs untuk mengambil semua data mahasiswa

--- app/src/main/java/com/example/activity10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/activity10/repository/RepositoryMhs.kt
@@ -1,7 +1,12 @@
 package com.example.activity10.repository
 
 import com.example.activity10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    //Dari sini
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+
 }
