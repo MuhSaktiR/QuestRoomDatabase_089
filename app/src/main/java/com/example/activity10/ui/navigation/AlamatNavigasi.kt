2fdbf8d4fc493a12c4b0c5b325111471fd9d6922
MuhSaktiR <seken876@gmail.com>
2024-12-16 20:15:35 +0700

    Menambahkan implementasi AlamatNavigasi untuk Home, Detail, dan Update

--- app/src/main/java/com/example/activity10/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/activity10/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,20 @@ package com.example.activity10.ui.navigation
 interface AlamatNavigasi {
     val route: String
 }
+
+//Dari Sini yang berubah
+object DestinasiHome : AlamatNavigasi {
+    override val route = "home"
+}
+
+object DestinasiDetail : AlamatNavigasi {
+    override val route = "detail"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
+
+object DestinasiUpdate : AlamatNavigasi {
+    override val route = "update"
+    const val NIM = "nim"
+    val routesWithArg = "$route/{$NIM}"
+}
